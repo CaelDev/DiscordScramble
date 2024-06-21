7a4732790cffe798ac73fd6b0e922e5f1ad902ae
UpByTheStars <upbythestars@gmail.com>
2024-06-21 18:35:20 -0400

    embeds, stat saving, admin-only commands, bug fixing

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
 # TO DO:
 # - setchannel only usable by admins
-# - make command responses look nicer
-# - add user personal stats
 
 
 # load bot token from hidden file
@@ -32,6 +30,9 @@ bot = commands.Bot(command_prefix="", intents=intents)
 # basic functions for bot startup
 @bot.event
 async def on_ready():
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.listening, name="the waves")
+    )
     checkWordExpire.start()  # start background loop
     await bot.tree.sync()
     print(f"Logged in as {bot.user.name} ({bot.user.id})")
@@ -85,16 +86,31 @@ def solve(serverid, userid, guess):
     if data[serverid]["currentWord"] == "" or data[serverid]["expires"] < int(
         time.time()
     ):  # make sure there is a word active
-        return "No word currently!"
-    if (
+        embed = discord.Embed(
+            title="No word",
+            description="There is currently no active word, please try again later",
+            color=0xFF0000,
+        )
+        return embed
+    elif (
         int(time.time()) < int(data[serverid]["lastGuess"]) + 30
     ):  # make sure guess isnt on cooldown
-        return f"Guess on cooldown for {int(data[serverid]['lastGuess']) + 30 - int(time.time())} more seconds!"
-    if guess != data[serverid]["currentWord"]:  # check if the guess is incorrect
+        embed = discord.Embed(
+            title="Cooldown",
+            description=f"Guess on cooldown for {int(data[serverid]['lastGuess']) + 30 - int(time.time())} more seconds!",
+            color=0xFFD60A,
+        )
+        return embed
+    elif guess != data[serverid]["currentWord"]:  # check if the guess is incorrect
         data[serverid]["lastGuess"] = int(time.time())  # add cooldown
         with open("servers.json", "w") as f:  # write the new data to the database
             f.write(json.dumps(data, indent=3))
-        return "Incorrect guess! 30 second guess cooldown starts now!"
+        embed = discord.Embed(
+            title="Incorrect",
+            description="Incorrect guess! 30 second guess cooldown starts now!",
+            color=0xFF0000,
+        )
+        return embed
     else:  # if the guess was correct:
         data[serverid]["lastGuess"] = 0  # reset guess cooldown
         data[serverid]["currentWord"] = getRandomWord(
@@ -104,22 +120,56 @@ def solve(serverid, userid, guess):
             int(time.time()) + 1 * 60 * 15
         )  # 15 min expiration time
         data[serverid]["solved"] += 1
-    with open("servers.json", "w") as f:  # write the new data to the database
-        f.write(json.dumps(data, indent=3))
+        with open("servers.json", "w") as f:  # write the new data to the database
+            f.write(json.dumps(data, indent=3))
 
-    x = list(data[serverid]["currentWord"])
-    random.shuffle(x)
-    return (
-        "Correct! New word has been generated: **"
-        + "".join(x)
-        + f"** (expires <t:{data[serverid]['expires']}:R>)"
-    )
+        x = list(data[serverid]["currentWord"])
+        random.shuffle(x)
+        embed = discord.Embed(
+            title="Correct!",
+            description=f"The word was {guess}. New word has been generated: **"
+            + "".join(x)
+            + f"** (expires <t:{data[serverid]['expires']}:R>)",
+            color=0x0AFF54,
+        )
+        addPoint(userid, guess)
+        return embed
+
+
+def addPoint(userid, word):
+    userid = str(userid)
+    with open("users.json") as f:
+        data = json.loads(f.read())
+    if str(userid) not in data:
+        data.update({str(userid): {"correct": 1, "words": [word]}})
+    else:
+        data[userid]["correct"] += 1
+        if word not in data[userid]["words"]:
+            data[userid]["words"].append(word)
+    with open("users.json", "w") as f:
+        f.write(json.dumps(data, indent=3))
 
 
 def getStats(serverid):  # gets server solve stats (pretty basic right now)
     with open("servers.json") as f:
         data = json.loads(f.read())
-    return f"The server has solved **{data[str(serverid)]['solved']} scrambles!**"
+    embed = discord.Embed(
+        title="Server Stats",
+        description=f"The server has solved **{data[str(serverid)]['solved']} scrambles!**",
+        color=0xFFD60A,
+    )
+    return embed
+
+
+def getPersonalStats(userid, username):
+    with open("users.json") as f:
+        data = json.loads(f.read())
+    embed = discord.Embed(
+        title=f"{username}'s Stats",
+        description=f"Questions solved: {data[str(userid)]['correct']} \n{len(data[str(userid)]['words'])} / 146600 possible words solved",  # possible words determined by filtering database for words between 4 and 8 chars
+        color=0xFFD60A,
+    )
+    return embed
 
 
 def getCommandChannel(serverid):
@@ -139,33 +189,62 @@ async def unscramble(interaction, unscrambled: str):  # handles slash command us
         the unscrambled word
     """
     if interaction.channel.id != getCommandChannel(interaction.guild.id):
+        embed = discord.Embed(
+            title="Error",
+            description=f"Wrong channel! Use <#{getCommandChannel(interaction.guild.id)}>",
+            color=0xFF0000,
+        )
         await interaction.response.send_message(
-            f"Wrong channel! Use <#{getCommandChannel(interaction.guild.id)}>",
+            embed=embed,
             ephemeral=True,
         )
     else:
         await interaction.response.send_message(
-            solve(interaction.guild.id, interaction.user.id, unscrambled)
+            embed=solve(interaction.guild.id, interaction.user.id, unscrambled)
         )
 
 
 # creates slash command for stat retrieval
-@bot.tree.command(name="stats", description="Display server unscramble stats")
-async def stats(interaction):  # handles slash usage
-    await interaction.response.send_message(getStats(interaction.guild.id))
+@bot.tree.command(name="serverstats", description="Display server unscramble stats")
+async def serverstats(interaction):  # handles slash usage
+    await interaction.response.send_message(embed=getStats(interaction.guild.id))
+
+
+@bot.tree.command(name="stats", description="Display personal unscramble stats")
+async def stats(interaction):
+    await interaction.response.send_message(
+        embed=getPersonalStats(interaction.user.id, interaction.user.name)
+    )
 
 
 @bot.tree.command(
     name="setchannel",
     description="Sets the current channel as the channel to register unscrambles",
 )
+@commands.has_permissions(manage_messages=True)
 async def setChannel(interaction):
-    with open("servers.json") as f:
-        data = json.loads(f.read())
-    data[str(interaction.guild.id)]["wordChannel"] = interaction.channel.id
-    with open("servers.json", "w") as f:
-        f.write(json.dumps(data, indent=3))
-    await interaction.response.send_message("Done!")
+    if (
+        interaction.user.top_role.permissions.administrator
+        or interaction.user.guild_permissions.administrator
+    ):
+        with open("servers.json") as f:
+            data = json.loads(f.read())
+        data[str(interaction.guild.id)]["wordChannel"] = interaction.channel.id
+        with open("servers.json", "w") as f:
+            f.write(json.dumps(data, indent=3))
+        embed = discord.Embed(
+            title="Success",
+            description=f"Channel has been set to <#{interaction.channel.id}>",
+            color=0x0AFF54,
+        )
+        await interaction.response.send_message(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="Error",
+            description=f"Only admins can use this command!",
+            color=0xFF0000,
+        )
+        await interaction.response.send_message(embed=embed)
 
 
 @tasks.loop(seconds=15)  # loops every 15 seconds
@@ -192,10 +271,13 @@ async def checkWordExpire():  # check if a server has expired words, and if so w
                         f.write(json.dumps(data, indent=3))
 
                     channel = bot.get_channel(data[i]["wordChannel"])
-                    await channel.send(
-                        "New word has been generated: "
-                        + "".join(shuffle(list(data[i]["currentWord"])))
+                    embed = discord.Embed(
+                        title="New Word",
+                        description="New word has been generated: "
+                        + "".join(shuffle(list(data[i]["currentWord"]))),
+                        color=0xFFD60A,
                     )
+                    await channel.send(embed=embed)
 
 
 bot.run(TOKEN)  # run the bot
